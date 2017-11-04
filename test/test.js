63f606c1993dbf87ff5fd03defc1e17bf4b7b253
liuyanhao <374659635@qq.com>
2017-11-04 10:36:37 +0800

    xiugai

--- test/test.js
+++ test/test.js
@@ -1,52 +1,20 @@
-class Vue {
-    constructor(options) {
-        this._data = options.data;
-        this.observer(this._data, options.render)
-        this._proxy(this._data,options.render);
-    }
-    observer(value, cb) {
-        var ctx = this;
-        Object.keys(value).forEach(key => {
-            ctx.defineReactive(value, key, value[key], cb);
-        })
-    }
-    defineReactive(obj, key, value, cb) {
-        Object.defineProperty(obj, key, {
-            enumerable: true,
-            configurable: true,
-            get() {
+// 定时器
+function timer(params) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            // resolve('resolve');
+            reject('reject');
+        }, params);
+    })
+}
 
-            },
-            set(newValue) {
-                cb(key,newValue);
-            }
-        })
-    }
-    _proxy(data,cb) {
-        var ctx = this;
-        Object.keys(data).forEach(key => {
-            Object.defineProperty(ctx, key,{
-                enumerable: true,
-                configurable: true,
-                get(){
-                    return ctx._data[key];
-                },
-                set(newValue){
-                    ctx._data[key] = newValue;
-                }
-            })
-        })
+// async/await
+async function fn(params) {
+    try {
+        var value = await timer(params);
+    } catch (error) {
+        console.log(error);        
     }
 }
 
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        text: 'text',
-        text2: 'text2'
-    },
-    render(key,newValue) {
-        console.log(key+' is '+newValue);
-    }
-})
+fn(1000); //执行
