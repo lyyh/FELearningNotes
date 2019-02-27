02c9b1a7d8b4e22ca8929d753b95155501ee06e5
anserliu <anserliu@tencent.com>
2019-02-27 10:22:33 +0800

    update throttle

--- part 4/前端高级技巧/函数节流防抖/throttle.js
+++ part 4/前端高级技巧/函数节流防抖/throttle.js
@@ -1,18 +1,63 @@
 // 简单的节流函数
-function throttle(fn, gapTime) {
-	let lastTime = null;
+// 时间戳方式（规定时间开始时触发）
+function throttle(fn, delay) {
+	let lastTime = 0;
 
-	return function () {
+	return function (...args) {
 		let nowTime = + new Date()
-		if (nowTime - lastTime > gapTime || !lastTime) {
-			fn();
+		if (nowTime - lastTime > delay) {
+			fn.apply(this,args);
 			lastTime = nowTime
 		}
 	}
 }
 
+// 定时器方式（规定时间结束时触发）
+function throttle1 (fn,delay) {
+	let timer = null
+	return function (...args) {
+		let that = this
+		if(!timer){
+			timer = setTimeout(()=>{
+				clearTimeout(timer)
+				timer = null
+				fn.apply(that,args)
+			},delay)
+		}
+	}
+}
+
+//时间戳+定时器版: 实现第一次触发可以立即响应,结束触发后也能有响应 (该版才是最符合实际工作需求)
+//该版主体思路还是时间戳版,定时器的作用仅仅是执行最后一次回调
+function throttle2 (fn,delay) {
+	let timer = null
+	let previous = 0;
+	return function (...args) {
+		let that = this
+		let now = Date.now()
+		let remaining = delay - (now - previous)
+		clearTimeout(timer)
+		if(remaining<=0){
+			// 立即执行
+			fn.apply(that,args)
+			previous = Date.now()
+		}else{
+			timer = setTimeout(()=>{
+				fn.apply(that,args)
+			},remaining)
+		}
+	}
+}
 let fn = ()=>{
 	console.log('boom')
 }
+let fn1 = ()=>{
+	console.log('boom1')
+}
+let fn2 = ()=>{
+	console.log('boom2')
+}
 
-setInterval(throttle(fn,1000),10)
+setInterval(throttle(fn,1000),10) // 开始时间执行
+setInterval(throttle1(fn1,1000),10) // 结束时间执行
+setInterval(throttle2(fn2,1000),10) // 第一次触发可以立即响应,结束触发后也能有响应
